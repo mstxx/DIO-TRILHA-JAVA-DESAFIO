ca5e5ef324e21a8b92f225ea786d5362efd73a2b
matheusrocha2398 <matheusrocha2398@penguin>
2023-09-18 14:03:43 -0300

    Segundo Commit com a conclusão da tarefa de criação de um emulador de conta bancaria.

--- BancoConta/src/terminalBanco/ContaTerminal.java
+++ BancoConta/src/terminalBanco/ContaTerminal.java
@@ -1,8 +1,35 @@
 package terminalBanco;
 
+import java.util.Scanner;
+
 public class ContaTerminal {
 
 	public static void main(String [] args) throws Exception {
-		System.out.println("Hellow World.");
+		//TODO:Conhecer e ipmortar a classe Scanner
+		//Exibir as mensagens para o nosso usuario
+		//Obter pela classe Scanner os valores digitados no Terminal
+		//Exibir a mensagem da conta criada
+		
+		Scanner scan  = new Scanner(System.in);
+		
+		String nomeCliente;
+		String agência;
+		int conta;
+		double saldo;	
+		
+		System.out.print("Digite o seu nome: ");
+		nomeCliente = scan.next();
+		System.out.print("Digite o número da agência mais próxima de você: ");
+		agência = scan.next();
+		System.out.print("Digite o número da conta que você deseja: ");
+		conta = scan.nextInt();
+		System.out.print("Qual o valor para seu primeiro depósito? ");
+		saldo = scan.nextInt();
+		
+		
+		System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta "
+							+ "em nosso banco, sua agência é " + agência + ", conta " 
+							+ conta + " e seu saldo é de R$ " + saldo + ".");
+	
 	}
 }
